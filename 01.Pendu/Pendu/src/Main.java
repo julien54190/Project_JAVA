0e0a9b1a3a76f13014af763468359475b5c7f824
Julien Lucadello <lucadello.julien@hotmail.fr>
2024-11-08 14:05:20 +0100

    amélioration code et ajout de la doc

--- 01.Pendu/Pendu/src/Main.java
+++ 01.Pendu/Pendu/src/Main.java
@@ -2,42 +2,58 @@ import comDymaGame.GuessGame;
 import java.util.Random;
 import java.util.Scanner;
 
+/**
+ * Class of the entrypoint of the Guess Game.
+ */
 public class Main {
 
+    /**
+     * Entry point of the Guess Game. Contains the main algorithm of the game.
+     */
     public static void main(String[] args) {
-        final var scanner = new Scanner(System.in);
         final var random = new Random();
         final var words = "abuser crottes fleches continental babiole etoile bougie coup coeur malade".split(" ");
-        final var wordToGuess = words[random.nextInt(words.length)];
-        final var game = new GuessGame(wordToGuess, 10);
+        final var lifePoints = 10;
+        var wordToGuess = words[random.nextInt(words.length)];
+        var game = new GuessGame(wordToGuess, lifePoints);
 
         System.out.println("Début du jeu.");
 
         while(true) {
             System.out.println(game);
-            System.out.println("Entrez une lettre");
-            final var letter = scanLetter(scanner);
+            final var letter = scanLetter("Entrez une lettre : ");
 
             game.guessLetter(letter);
-
             if (game.isLost()) {
-                System.out.println(game);
-                System.out.println("Vous avez perdu! Le mot était : " + wordToGuess);
-                break;
+                System.out.println("Perdu !");
             }
-
             if (game.isWon()) {
-                System.out.println(game);
-                System.out.println("Vous avez gagné!");
-                break;
+                System.out.println("Gagné !");
             }
-
-
+            if (game.isWon() || game.isLost()) {
+                System.out.println(game);
+                var replayAnswer = scanLetter("Rejouer ? (y/n)");
+                if (replayAnswer == 'y' || replayAnswer == 'Y' || replayAnswer == 'o' || replayAnswer == 'O') {
+                    wordToGuess = words[random.nextInt(words.length-1)];
+                    game = new GuessGame(wordToGuess, lifePoints);
+                } else {
+                    break;
+                }
+            }
         }
     }
 
-    private static char scanLetter(final Scanner scanner) {
-        return scanner.nextLine().charAt(0);
+    private static char scanLetter(String question) {
+        final var scanner = new Scanner(System.in);
+        Character letter = null;
+        do {
+            System.out.println(question);
+            var input = scanner.nextLine();
+            if (input.length() == 1) {
+                letter = input.charAt(0);
+            }
+        } while (letter == null);
+        return letter;
     }
 
 }
